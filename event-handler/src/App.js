68e5cb34b8d1af54ca2e6ce51f957e40b908e68f
João Gabriel Antunes <jg.antunes.medeiros@gmail.com>
2021-09-06 16:28:41 -0300

    Adding events list

--- event-handler/src/App.js
+++ event-handler/src/App.js
@@ -5,11 +5,48 @@ import moment from 'moment';
 
 const localizer = momentLocalizer(moment);
 
+const myEvents = [{
+    id: 0,
+    title: 'Long Event',
+    start: new Date(2021, 8, 7, 9, 10),
+    end: new Date(2021, 8, 7, 11, 45),
+  },
+  {
+  id: 1,
+    title: 'Chores',
+    start: new Date(2021, 8, 5, 12, 0),
+    end: new Date(2021, 8, 5, 13, 30),
+  },
+  {
+  id: 2,
+    title: 'Lunch with Lily',
+    start: new Date(2021, 8, 7),
+    end: new Date(2021, 8, 7),
+  },
+  {
+  id: 3,
+    title: 'Movie Night',
+    start: new Date(2021, 8, 6, 15, 0),
+    end: new Date(2021, 8, 6, 18, 0),
+  },
+  {
+  id: 4,
+    title: 'Pair programming with Tom',
+    start: new Date(2021, 8, 8),
+    end: new Date(2021, 8, 8),
+  },
+]
+
 function App() {
   return (
-    <div className="App">
-      <Calendar localizer={localizer} events={[]}/>
+    <>
+    <div className="addButton">
+      Hi
     </div>
+    <div className="App">
+      <Calendar localizer={localizer} events={myEvents} showMultiDayTimes step={30}/>
+    </div>
+    </>
   );
 }
 
