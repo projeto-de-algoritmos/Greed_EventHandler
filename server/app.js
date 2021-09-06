1ab17a80c85ee7fd081830c84de476ca317ccfdb
lorryaze <lorrany90@gmail.com>
2021-09-06 18:38:00 -0300

    Solving problem

--- server/app.js
+++ server/app.js
@@ -1,9 +1,8 @@
+const { constants } = require('buffer');
 const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 3000;
-
-var events = []
 
 class Event {
   constructor(ei, ee, name) {
@@ -13,27 +12,30 @@ class Event {
   }
 }
 
-function addEvent(ei, ee, name) {
+function compareEvent(a, b) {
+  return a.ee - b.ee;
+}
+
+function sortEvent(events) {
+  events.sort(compareEvent);
+}
+
+function solve(events) {
+
+  sortEvent(events);
   
-  const ev = new Event(ei, ee, name);
-  var eventsToBeOrdered = events.push(ev);
-  return eventsToBeOrdered;
+  const ans = [events[0]];
+  let last = events[0].ee;
+
+  for(let i = 1; i < events.lenght; i++) {
+    if(ord.ei < last) continue;
+    ans.push(ord[i]);
+    last = ord[i].ee;
+  }
+
+  return ans;
 
 }
-
-function removeEvent(name, pos) {
-
-  var item = events.indexOf(name);
-  var removedItens = events.splice(item, pos);
-  return removedItens;
-
-}
-
-function solve() {
-  
-}
-
-
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -44,13 +46,20 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 
-  addEvent(13, 15, 'Ir pro marlon');
-  addEvent(13, 15, 'Ir pro marlo');
-  addEvent(13, 15, 'Ir pro marl');
-  addEvent(13, 16, 'Code');
-  removeEvent('Code', 1);
-
+  const events = [
+    new Event(13, 15, '1'),
+    new Event(12, 15, '2'),
+    new Event(14, 20, '3'),
+    new Event(11, 17, '4'),
+    new Event(11, 20, '5'),
+  ];
   
-  console.log(events[0]);
-  console.log(events[1]);
+  console.log("MEU ARRAY");
+  console.log(events);
+  
+  var ans = [];
+  ans = solve(events);
+  console.log("RESPOSTA");
+  console.log(ans);
+
 });
